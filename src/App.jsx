dff606004c38eb366e26effa80a1ed84dd2bad98
Dayan Arango <dayaara6@gmail.com>
2022-05-09 12:40:24 -0400

    creando el select

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { Container, Grid, Typography} from '@mui/material'
+import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
 function App() {
-
   return (
     <Container>
       <header>
-        <Typography align='center' marginY={5} component='h1' 
-        variant='h3'>
+        <Typography align="center" marginY={5} component="h1" variant="h3">
           Buscador de Noticias
         </Typography>
       </header>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={12} md={6} lg={4}>
+          <Formulario />
+        </Grid>
+      </Grid>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,41 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  Button,
+  Box,
+  MenuItem,
+} from "@mui/material";
+const CATEGORIAS = [
+  { value: "general", label: "General" },
+  { value: "business", label: "Negocios" },
+  { value: "entertainment", label: "Entretenimiento" },
+  { value: "health", label: "Salud" },
+  { value: "science", label: "Ciencia" },
+  { value: "sports", label: "Deportes" },
+  { value: "technology", label: "Tecnología" },
+];
+
+const Formulario = () => {
+  return (
+    <form>
+      <FormControl fullWidth>
+        <InputLabel>Categoría</InputLabel>
+        <Select label="Categoría">
+          {CATEGORIAS.map((categoria) => (
+            <MenuItem key={categoria.value} value={categoria.value}>
+              {categoria.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <Box sx={{ marginTop: 2 }}>
+          <Button fullWidth variant="contained" color="secondary">
+            Buscar Noticias
+          </Button>
+        </Box>
+      </FormControl>
+    </form>
+  );
+};
+
+export default Formulario;
