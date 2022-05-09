fedad2d4d4d489a65e49a8bbf77586899e54fa1a
Dayan Arango <dayaara6@gmail.com>
2022-05-09 13:10:52 -0400

    trayendo los datos de la api

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,22 +1,34 @@
-import { createContext, useState, useEffect } from "react"
+import axios from "axios";
+import { createContext, useState, useEffect } from "react";
 
-const NoticiasContext = createContext()
+const NoticiasContext = createContext();
 const NoticiasProvider = ({ children }) => {
-    const [ categoria, setCategoria ] = useState("general")
-    const handleChangeCategoria = e => {
-        setCategoria(e.target.value)
-    }
-    
-    return (
-        <NoticiasContext.Provider 
-        value={{
-            categoria,
-            handleChangeCategoria
-        }}
-        >
-            {children}
-        </NoticiasContext.Provider>
-    )
-}
-export { NoticiasProvider }
-export default NoticiasContext
+  const [categoria, setCategoria] = useState("general");
+  const [ noticias, setNoticias ] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ch&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
+      const { data } = await axios.get(url);
+      setNoticias(data.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
+
+  const handleChangeCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  return (
+    <NoticiasContext.Provider
+      value={{
+        categoria,
+        handleChangeCategoria,
+      }}
+    >
+      {children}
+    </NoticiasContext.Provider>
+  );
+};
+export { NoticiasProvider };
+export default NoticiasContext;
