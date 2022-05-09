9dac148f877aa4b64068f73a2afce22488d20283
Dayan Arango <dayaara6@gmail.com>
2022-05-09 12:22:37 -0400

    Creando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Container, Grid, Typography} from '@mui/material'
+function App() {
+
+  return (
+    <Container>
+      <header>
+        <Typography align='center' marginY={5} component='h1' 
+        variant='h3'>
+          Buscador de Noticias
+        </Typography>
+      </header>
+    </Container>
+  )
+}
+
+export default App
